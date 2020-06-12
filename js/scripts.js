7190bdb69117f98953a2e9a9e08594a480963c38
Jordan <jpkronquist@gmail.com>
2020-06-12 12:52:18 -0700

    add js folder to project folder.

---
+++ js/scripts.js
@@ -0,0 +1,167 @@
+$(document).ready(function(){
+
+    $("h5#card1").click(function(){
+        $("h5#card1").hide();
+        $("p#def-card1").show();
+    });
+    $("p#def-card1").click(function(){
+        $("h5#card1").show();
+        $("p#def-card1").hide();
+    });
+    
+    $("h5#card2").click(function(){
+        $("h5#card2").hide();
+        $("p#def-card2").show();
+    });
+    $("p#def-card2").click(function(){
+        $("h5#card2").show();
+        $("p#def-card2").hide();
+    });
+    
+    $("h5#card3").click(function(){
+        $("h5#card3").hide();
+        $("p#def-card3").show();
+    });
+    $("p#def-card3").click(function(){
+        $("h5#card3").show();
+        $("p#def-card3").hide();
+    });
+    
+    
+    $("h5#card4").click(function(){
+        $("h5#card4").hide();
+        $("p#def-card4").show();
+    });
+    $("p#def-card4").click(function(){
+        $("h5#card4").show();
+        $("p#def-card4").hide();
+    });
+    
+    $("h5#card5").click(function(){
+        $("h5#card5").hide();
+        $("p#def-card5").show();
+    });
+    $("p#def-card5").click(function(){
+        $("h5#card5").show();
+        $("p#def-card5").hide();
+    });
+    
+    $("h5#card6").click(function(){
+        $("h5#card6").hide();
+        $("p#def-card6").show();
+    });
+    $("p#def-card6").click(function(){
+        $("h5#card6").show();
+        $("p#def-card6").hide();
+    });
+    
+    
+    $("h5#card7").click(function(){
+        $("h5#card7").hide();
+        $("p#def-card7").show();
+    });
+    $("p#def-card7").click(function(){
+        $("h5#card7").show();
+        $("p#def-card7").hide();
+    });
+    
+    $("h5#card8").click(function(){
+        $("h5#card8").hide();
+        $("p#def-card8").show();
+    });
+    $("p#def-card8").click(function(){
+        $("h5#card8").show();
+        $("p#def-card8").hide();
+    });
+    
+    $("h5#card9").click(function(){
+        $("h5#card9").hide();
+        $("p#def-card9").show();
+    });
+    $("p#def-card9").click(function(){
+        $("h5#card9").show();
+        $("p#def-card9").hide();
+    });
+    $("h5#card10").click(function(){
+        $("h5#card10").hide();
+        $("p#def-card10").show();
+    });
+    $("p#def-card10").click(function(){
+        $("h5#card10").show();
+        $("p#def-card10").hide();
+    });
+    
+    $("h5#card11").click(function(){
+        $("h5#card11").hide();
+        $("p#def-card11").show();
+    });
+    $("p#def-card11").click(function(){
+        $("h5#card11").show();
+        $("p#def-card11").hide();
+    });
+    
+    $("h5#card12").click(function(){
+        $("h5#card12").hide();
+        $("p#def-card12").show();
+    });
+    $("p#def-card12").click(function(){
+        $("h5#card12").show();
+        $("p#def-card12").hide();
+    });
+    $("h5#card13").click(function(){
+        $("h5#card13").hide();
+        $("p#def-card13").show();
+    });
+    $("p#def-card13").click(function(){
+        $("h5#card13").show();
+        $("p#def-card13").hide();
+    });
+    
+    $("h5#card14").click(function(){
+        $("h5#card14").hide();
+        $("p#def-card14").show();
+    });
+    $("p#def-card14").click(function(){
+        $("h5#card14").show();
+        $("p#def-card14").hide();
+    });
+    
+    $("h5#card15").click(function(){
+        $("h5#card15").hide();
+        $("p#def-card15").show();
+    });
+    $("p#def-card15").click(function(){
+        $("h5#card15").show();
+        $("p#def-card15").hide();
+    });
+    $("h5#card16").click(function(){
+        $("h5#card16").hide();
+        $("p#def-card16").show();
+    });
+    $("p#def-card16").click(function(){
+        $("h5#card16").show();
+        $("p#def-card16").hide();
+    });
+    
+    $("h5#card17").click(function(){
+        $("h5#card17").hide();
+        $("p#def-card17").show();
+    });
+    $("p#def-card17").click(function(){
+        $("h5#card17").show();
+        $("p#def-card17").hide();
+    });
+    
+    $("h5#card18").click(function(){
+        $("h5#card18").hide();
+        $("p#def-card18").show();
+    });
+    $("p#def-card18").click(function(){
+        $("h5#card18").show();
+        $("p#def-card18").hide();
+    });
+    
+    
+    
+    });
+    
